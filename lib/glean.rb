2822919f1b16cab2d08d5051e6989659ab5cb7a5
John Britton <public@johndbritton.com>
2013-04-13 15:49:37 +0200

    add Hashie

--- lib/glean.rb
+++ lib/glean.rb
@@ -4,5 +4,6 @@ require 'glean/dataset'
 require 'git'
 require 'toml'
 require 'octokit'
+require 'hashie/mash'
 # Add requires for other files you add to your project here, so
 # you just need to require this one file in your bin file
