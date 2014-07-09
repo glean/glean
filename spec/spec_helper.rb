9db9f30a6e8cb87dcda14fa084fcf1b613306310
Simeon F. Willbanks <sfw@simeonfosterwillbanks.com>
2014-07-08 19:14:22 -0700

    rspec treat_symbols_as_metadata_keys_with_true_values is now true by default

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -10,5 +10,4 @@ end
 RSpec.configure do |c|
   # so we can use :vcr rather than :vcr => true;
   # in RSpec 3 this will no longer be necessary.
-  c.treat_symbols_as_metadata_keys_with_true_values = true
 end
